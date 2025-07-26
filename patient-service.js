e8178f75b038005f8bc599a3d8fa22a0f42b1b88
matheesh-k <kmatheesh396@gmail.com>
2025-07-26 10:57:41 +0530

    Update patient-service.js

--- patient-service.js
+++ patient-service.js
@@ -1,7 +1,6 @@
 const express = require('express');
     const app = express();
     const port = process.env.PORT || 3000;
-    
     app.use(express.json());
     
     // In-memory data store (replace with a database in a real application)
